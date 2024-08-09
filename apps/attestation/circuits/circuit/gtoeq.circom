c07200d43e1e3c520f73b483ab5900dbcf2df449
Ewanretor Giwa-Okugbe <ewanretorokugbe@gmail.com>
2024-08-10 00:57:33 +0100

    upgrades circuit schema and added generation for externally provable proof, publicSignals and verificationKey

--- apps/attestation/circuits/circuit/gtoeq.circom
+++ apps/attestation/circuits/circuit/gtoeq.circom
@@ -4,13 +4,15 @@ include "../../node_modules/circomlib/circuits/comparators.circom";
 
 template GreaterThanOrEqual(n) {
     signal input in[2];
+    signal output against;
     signal output out;
     
+    against <== in[1];
     component gte = GreaterEqThan(n);
     gte.in[0] <== in[0];
     gte.in[1] <== in[1];
 
-    out <== gte.out; 
+    out <== gte.out;
 }
 
 component main = GreaterThanOrEqual(16);
--- apps/attestation/test/utils/generatingProofs.ts
+++ apps/attestation/test/utils/generatingProofs.ts
@@ -45,20 +45,19 @@ export const generateProof = async (
       .split(",")
       .map((x: string | number | bigint | boolean) => BigInt(x).toString());
 
-    const a = [argv[0], argv[1]];
-    const b = [
+    const _pA = [argv[0], argv[1]];
+    const _pB = [
       [argv[2], argv[3]],
       [argv[4], argv[5]],
     ];
-    const c = [argv[6], argv[7]];
-    const Output = [];
+    const _pC = [argv[6], argv[7]];
+    const _pubSignals = [];
 
     for (let i = 8; i < argv.length; i++) {
-      Output.push(argv[i]);
+      _pubSignals.push(argv[i]);
     }
-    const Input = input1;
-    
-    return { a, b, c, Input, Output };
+
+    return { _pA, _pB, _pC, _pubSignals };
   } catch (err) {
     console.log(`Error:`, err);
     return {
@@ -68,10 +67,55 @@ export const generateProof = async (
   }
 };
 
-async function main() {
-  const res = await generateProof(21, 18, "gtoeq");
+export const generateGeneralProofs = async (
+  input0: number,
+  input1: number,
+  file: string
+): Promise<any> => {
+  console.log(`Generating age proof with inputs: ${input0}, ${input1}`);
 
-  return res;
+  // We need to have the naming scheme and shape of the inputs match the .circom file
+  const inputs = {
+    in: [input0, input1],
+  };
+
+  // Paths to the .wasm file and proving key
+  const wasmPath = path.join(
+    process.cwd(),
+    `./circuits/build/gtoeq_js/${file}.wasm`
+  );
+  const provingKeyPath = path.join(
+    process.cwd(),
+    `./circuits/keys/${file}_final.zkey`
+  );
+
+  try {
+    // Generate a proof of the circuit and create a structure for the output signals
+    const { proof, publicSignals } = await snarkjs.groth16.fullProve(
+      inputs,
+      wasmPath,
+      provingKeyPath
+    );
+
+    const verificationKey = await snarkjs.zKey.exportJson(provingKeyPath);
+
+    return { proof, publicSignals, verificationKey };
+  } catch (err) {
+    console.log(`Error:`, err);
+    return {
+      proof: "",
+      publicSignals: [],
+    };
+  }
+};
+
+type res = { res: any; res2: any };
+
+async function main(): Promise<res> {
+  const res = await generateProof(21, 18, "gtoeq");
+  const res2 = await generateGeneralProofs(21, 18, "gtoeq");
+
+  return { res, res2 };
 }
 
 main()
@@ -80,9 +124,48 @@ main()
     process.exitCode = 1;
   })
   .then((a) => {
-    fs.writeFile("./proof.json", JSON.stringify(a), (err) => {
+    const _a = a as res;
+    fs.writeFile("./proof.json", JSON.stringify(_a.res, null, 2), (err) => {
       if (err) throw err;
       console.log("The file has been saved!");
     });
+    return;
+  });
+
+main()
+  .catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  })
+  .then((a) => {
+    const _a = a as res;
+
+    fs.existsSync("keys")
+      ? console.log()
+      : fs.mkdir("keys", (err) => {
+          if (err) throw err;
+        });
+    fs.writeFile(
+      "./keys/proof.json",
+      JSON.stringify(_a.res2.proof, null, 2),
+      (err) => {
+        if (err) throw err;
+      }
+    );
+    fs.writeFile(
+      "./keys/public.json",
+      JSON.stringify(_a.res2.publicSignals, null, 2),
+      (err) => {
+        if (err) throw err;
+      }
+    );
+    fs.writeFile(
+      "./keys/verification_key.json",
+      JSON.stringify(_a.res2.verificationKey, null, 2),
+      (err) => {
+        if (err) throw err;
+        console.log("The group of files have been saved to the keys folder!");
+      }
+    );
     return;
   });
